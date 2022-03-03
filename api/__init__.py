bbb6a30244f2494e6ccc5299c5dcebfff287cb4e
jurimullercloud <jurimuller.cloud@gmail.com>
2022-03-03 12:01:43 +0400

    Fixed Dockerfile.db

--- api/__init__.py
+++ api/__init__.py
@@ -2,12 +2,19 @@ from os import environ
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from os import environ
+import os
 
 app = Flask(__name__)
 cors = CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DB_URI")
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+POSTGRES_DB = os.environ["POSTGRES_DB"]
+PG_HOST = os.environ["PG_HOST"]
+
+DB_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{PG_HOST}:5432/{POSTGRES_DB}"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = environ.get("JWT_SECRET_KEY")
 
